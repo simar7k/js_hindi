8a3b37becff5e3534d4d4572c5c2fb898b3470d7
simar7k <161850318+simar7k@users.noreply.github.com>
2024-05-19 18:32:06 +0000

    a commit done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 21001664
+let accountEmail = "simar@email.com"
+var accountPassword = "simkaur"
+accountCity = "Delhi"
+let accountCountry;
+//accountId = 456
+console.table([accountId,accountEmail,accountCity])
+//use const and let
+//avoid using var as it is problematic in block and function scope
+console.log(accountCountry)
